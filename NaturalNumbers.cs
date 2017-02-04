0cef8d20f8fec32985413ef1dcac230c4ef83a97
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-03 22:09:39 -0600

    Add files via upload

---
+++ NaturalNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+            while (i < n)
+            {
+                i = i + 1;
+                Console.Write("[{0}] ",i);
+       
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
