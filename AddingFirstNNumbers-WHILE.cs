301ffdaf14dc7d71301c5c8b7e11a36ea82f12f8
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-06 19:09:00 -0600

    Add files via upload

---
+++ AddingFirstNNumbers-WHILE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingFirstNNumers_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int w = 0;
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+            while (i < n)
+            {
+                i = i + 1;
+                w = w + i;
+
+            }
+            Console.Write(w);
+            Console.ReadKey();
+        }
+    }
+}
