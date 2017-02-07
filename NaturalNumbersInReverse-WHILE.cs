5b15a250a7184f04da3625915e84d4433c3ba124
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-06 23:25:34 -0600

    Add files via upload

---
+++ NaturalNumbersInReverse-WHILE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            while (0 < n)
+            {
+                Console.Write("[{0}]", n);
+                n = n - 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
