d167b15528f0bee3354cfe7dd337f8308ebfcad6
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-06 23:35:19 -0600

    Add files via upload

---
+++ OnlyPositives-WHILE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyPositives_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Please enter a positive number:  ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n <= 0)
+            {
+                Console.Write("Please enter a number grater than zero:  ");
+                n = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("Wow finally... thank you for entering a positive number :v");
+            Console.ReadKey();
+        }
+    }
+}
